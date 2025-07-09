3325e58fed2d91c451138237c1d43cb662a4cfd0
emmanuelknafo <48259636+emmanuelknafo@users.noreply.github.com>
2025-07-09 19:10:14 -0400

    release v0.0.15

--- codex-cli/src/cli.tsx
+++ codex-cli/src/cli.tsx
@@ -113,7 +113,7 @@ const cli = meow(
                                into context and applies a batch of edits in one go. Incompatible
                                with all other flags, except for --model.
 
-  Examples - (Emmanuel version 0.0.14)
+  Examples - (Emmanuel version 0.0.15)
     $ codex "Write and run a python program that prints ASCII art"
     $ codex -q "fix build issues"
     $ codex completion bash
